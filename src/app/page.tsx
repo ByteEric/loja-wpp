2991d40907c1846673d5118bb9ca3de2d0163f96
tiago-mends <tiagomendesdev@gmail.com>
2024-03-08 00:28:59 -0300

    ADDED: Header and Footer components to the page UPDATED: Rendered content within the Home component to include Header, "Products" content, Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-	return <ThemeToggle />;
+	return (
+		<div className="w-full max-w-4xl mx-auto">
+			<Header />
+			<div className="mx-3">Products</div>
+			<Footer />
+		</div>
+	);
 }
