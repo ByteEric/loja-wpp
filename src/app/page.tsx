7416bc8a59aa7429085410d7481f7669c75bdb21
tiago-mends <tiagomendesdev@gmail.com>
2024-03-08 00:14:03 -0300

    ADDED: ThemeToggle component - Imported and added the ThemeToggle component to the pagex file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-	return <Button>Button</Button>;
+	return <ThemeToggle />;
 }
