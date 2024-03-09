b20ff7b599114a1292b926f036cebd5edd91b726
tiago-mends <tiagomendesdev@gmail.com>
2024-03-08 23:26:38 -0300

    UPDATED: Added remotePatterns to next.config.mjs
    - Added a new remotePatterns property to the images object in next.config.mjs
    - The remotePatterns property contains an array with a single object that specifies a protocol, hostname, and pathname for remote images
    - The protocol is set to "https", the hostname is set to "placeholder.com", and the pathname is set to "**" to match any path for the remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "placeholder.com",
+				pathname: "**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
